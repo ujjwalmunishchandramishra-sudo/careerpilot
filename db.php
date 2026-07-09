cc7d2bc929ea0996f418d290d449e33c4a451ec2
ujjwal mishra <ujjwalmunishchandramishra@gmail.com>
2026-07-09 11:11:46 +0530

    Update db.php
    Prepare project for GitHub

--- db.php
+++ db.php
@@ -1,12 +1,14 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "careerpilot";
+
+$host = "YOUR_HOST";
+$user = "YOUR_USERNAME";
+$password = "YOUR_PASSWORD";
+$database = "YOUR_DATABASE";
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Database Connection Failed");
 }
+
 ?>
